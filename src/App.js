8f77c7573b83b88b2819cb52932cc287e855b4b4
차누 <coldhailstone@chanuui-Macmini.local>
2022-12-30 17:30:58 +0900

    ~28 생성 버튼

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <div className="container">
+      <div className="container mt-3">
         <Outlet />
       </div>
     </>
--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Card from '../components/Card';
 
 function ListPage() {
     const [posts, setPosts] = useState([]);
@@ -15,10 +17,19 @@ function ListPage() {
 
     return (
         <div>
-            <h1>Blogs</h1>
+            <div className='d-flex justify-content-between'>
+                <h1>Blogs</h1>
+                <div>
+                    <Link to="/blogs/create" className='btn btn-success'>
+                        Create New
+                    </Link>
+                </div>
+            </div>
             {posts.map((post) => {
                 return (
-                    <div key={post.id}>{post.title}</div>
+                    <Card key={post.id} title={post.title}>
+                        <button>button</button>
+                    </Card>
                 )
             })}
         </div>
